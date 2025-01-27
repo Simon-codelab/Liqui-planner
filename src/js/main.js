af33719126ee91b6c9467a9184a54e5efa405358
STORMTROOPER\simon <simonknauf93@gmail.com>
2025-01-27 15:11:26 +0100

    promt title

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 "use strict";
+
+let Title = prompt("Titel", "z.B. Einkaufen");
+let Type = prompt("Typ", "einnahme");
+let Amount = prompt("Betrag", "z.B. 10,42");
+let Date = prompt("Datum", "jjjj-mm-tt");
+
+console.log(Title);
+console.log(Type);
+console.log(Amount);
+console.log(Date);
+
+
+//GesamtBilanz
+
+let einnahmen;
+let ausgaben;
+let bilanz;
