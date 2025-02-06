4ae835b6ba73723b0ccd10c0bf071cdbaeb6ba55
STORMTROOPER\simon <simonknauf93@gmail.com>
2025-02-06 12:47:12 +0100

    Bilanz ist positiv

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 
 let title_1 = prompt("Titel:");
 let typ_1 = prompt("Typ", "einnahme");
-let betrag_1 = prompt("Betrag (in Cent)");
+let betrag_1 = parseInt(prompt("Betrag (in Cent)"));
 let datum_1 = prompt("Datum", "jjjj-mm-tt");
 
 console.log('Title: ${titel_1}');
@@ -12,7 +12,7 @@ console.log('Datum: ${datum_1}');
 
 let title_2 = prompt("Titel", );
 let typ_2 = prompt("Typ", "einnahme");
-let betrag_2 = prompt("Betrag", "z.B. 100,00");
+let betrag_2 = parseInt(prompt("Betrag", "z.B. 100,00"));
 let datum_2 = prompt("Datum", "jjjj-mm-tt");
 
 console.log('Title: ${titel_2}');
@@ -25,4 +25,7 @@ console.log('Datum: ${datum_2}');
 
 let einnahmen;
 let ausgaben;
-let bilanz;
+let bilanz = betrag_1 + betrag_2;
+
+let positiv = bilanz >= 0;
+console.log('Bilanz ist positiv: ${positiv}');
