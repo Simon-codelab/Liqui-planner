8b96f92c38fb890ea6c0e06857ead5a7507a30e4
STORMTROOPER\simon <simonknauf93@gmail.com>
2025-02-06 12:36:58 +0100

    zweite beiträge

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,24 @@
 "use strict";
 
-let Title = prompt("Titel", "z.B. Einkaufen");
-let Type = prompt("Typ", "einnahme");
-let Amount = prompt("Betrag", "z.B. 10,42");
-let Date = prompt("Datum", "jjjj-mm-tt");
+let title_1 = prompt("Titel:");
+let typ_1 = prompt("Typ", "einnahme");
+let betrag_1 = prompt("Betrag (in Cent)");
+let datum_1 = prompt("Datum", "jjjj-mm-tt");
 
-console.log(Title);
-console.log(Type);
-console.log(Amount);
-console.log(Date);
+console.log('Title: ${titel_1}');
+console.log('Typ: ${typ_1}');
+console.log('Betrag: ${betrag_1} ct');
+console.log('Datum: ${datum_1}');
+
+let title_2 = prompt("Titel", );
+let typ_2 = prompt("Typ", "einnahme");
+let betrag_2 = prompt("Betrag", "z.B. 100,00");
+let datum_2 = prompt("Datum", "jjjj-mm-tt");
+
+console.log('Title: ${titel_2}');
+console.log('Typ: ${typ_2}');
+console.log('Betrag: ${betrag_2} ct');  
+console.log('Datum: ${datum_2}');
 
 
 //GesamtBilanz
