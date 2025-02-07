6c6ff5c2a53eef98bd43262647ddc69637b844ed
STORMTROOPER\simon <simonknauf93@gmail.com>
2025-02-07 12:54:11 +0100

    korriegiert

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 "use strict";
 
-let title_1 = prompt("Titel:");
+let titel_1 = prompt("Titel:");
 let typ_1 = prompt("Typ", "einnahme");
-let betrag_1 = parseInt(prompt("Betrag (in Cent)"));
+let betrag_1 = parseFloat(prompt("Betrag (in Cent)"));
 let datum_1 = prompt("Datum", "jjjj-mm-tt");
+
+let einnahmen = 0;
+let ausgaben = 0;
 
 console.log(`Title: ${titel_1}
     Typ: ${typ_1}
@@ -11,25 +14,33 @@ console.log(`Title: ${titel_1}
     Datum: ${datum_1}`
 );
 
-    
+if (typ_1 === "einnahme") {
+    einnahmen += betrag_1;
+} else {
+    ausgaben += betrag_1;
+}
 
-
-let title_2 = prompt("Titel", );
+let titel_2 = prompt("Titel:");
 let typ_2 = prompt("Typ", "einnahme");
-let betrag_2 = parseInt(prompt("Betrag", "z.B. 100,00"));
+let betrag_2 = parseFloat(prompt("Betrag (in Cent)"));
 let datum_2 = prompt("Datum", "jjjj-mm-tt");
 
 console.log(`Title: ${titel_2}
     Typ: ${typ_2}
     Betrag: ${betrag_2} ct
-    Datum: ${datum_2}`);
+    Datum: ${datum_2}`
+);
 
+if (typ_2 === "einnahme") {
+    einnahmen += betrag_2;
+} else {
+    ausgaben += betrag_2;
+}
 
-//GesamtBilanz
-
-let einnahmen;
-let ausgaben;
-let bilanz = betrag_1 + betrag_2;
-
+let bilanz = einnahmen - ausgaben;
 let positiv = bilanz >= 0;
-console.log('Bilanz ist positiv: ${positiv}');
+
+console.log(`Gesamteinnahmen: ${einnahmen} ct`);
+console.log(`Gesamtausgaben: ${ausgaben} ct`);
+console.log(`Bilanz: ${bilanz} ct`);
+console.log(`Bilanz ist positiv: ${positiv}`);
