88bf4fe9e9662442a4644061b84aabb5add8a02e
STORMTROOPER\simon <simonknauf93@gmail.com>
2025-02-24 14:05:01 +0100

    funktion

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,28 @@
 "use strict";
 
-let titel_1 = prompt("Titel:");
-let typ_1 = prompt("Typ", "einnahme");
-let betrag_1 = parseFloat(prompt("Betrag (in Cent)"));
-let datum_1 = prompt("Datum", "jjjj-mm-tt");
 
-let einnahmen = 0;
-let ausgaben = 0;
+let einnahmen = 0,
+    ausgaben = 0,
+    bilanz = 0,
+    titel, 
+    typ, 
+    betrag, 
+    datum;
 
-console.log(`Title: ${titel_1}
-    Typ: ${typ_1}
-    Betrag: ${betrag_1} ct
-    Datum: ${datum_1}`
+const eintrag_erfassen = function () {
+    titel = prompt("Titel:");
+    typ = prompt("Typ", "einnahme");
+    betrag= parseFloat(prompt("Betrag (in Cent)"));
+    datum= prompt("Datum", "jjjj-mm-tt");
+}
+
+console.log(`Title: ${titel}
+    Typ: ${typ}
+    Betrag: ${betrag} ct
+    Datum: ${datum}`
 );
 
+eintrag_erfassen();
 if (typ_1 === "einnahme") {
     einnahmen += betrag_1;
 } else {
@@ -37,7 +46,7 @@ if (typ_2 === "einnahme") {
     ausgaben += betrag_2;
 }
 
-let bilanz = einnahmen - ausgaben;
+let bilanz_0 = einnahmen - ausgaben;
 let positiv = bilanz >= 0;
 
 console.log(`Gesamteinnahmen: ${einnahmen} ct`);
